836c63147a6917e84765740a4591c270e8ce8133
Matheus Vinícius <matheusviniciusga@gmail.com>
2026-09-14 11:29:42 -0300

    organizando repositório

---
+++ pratica-financeiro/financeiro/src/test/java/com/pratica/api/financeiro/FinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratica.api.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
