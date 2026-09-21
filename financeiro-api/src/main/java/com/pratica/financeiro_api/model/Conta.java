27c3d8d7640a23ececc7629eeba5feb92bf5d8d7
Matheus Vinícius <matheusviniciusga@gmail.com>
2026-09-21 11:40:24 -0300

    add entidade conta

--- financeiro-api/src/main/java/com/pratica/financeiro_api/model/Conta.java
+++ financeiro-api/src/main/java/com/pratica/financeiro_api/model/Conta.java
@@ -2,11 +2,27 @@ package com.pratica.financeiro_api.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+
+@Entity 
 public class Conta {
 
-    Long id;
-    String titular;
-    BigDecimal saldo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titular;
+    private BigDecimal saldo;
 
-    
+    public Conta(Long id, String titular, BigDecimal saldo) {
+        this.id = id;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public Long getId() { return id; }
+    public String getTitular() { return titular; }
+    public BigDecimal getSaldo() { return saldo; }
 }
